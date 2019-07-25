cf7b623a0a3904591631639863115d58cf33de2c
zu1662 <zu1662@163.com>
2019-07-25 10:38:58 +0800

    add vscode setting.json code

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,3 +13,31 @@ module.exports = {
     browser: true,
   },
 }
+
+// vscode set eslint standard setting.json
+/** 
+{
+  "eslint.autoFixOnSave": true,
+  "eslint.validate": [
+      "javascript",
+      "javascriptreact",
+      "html",
+      "vue"
+  ],
+  "eslint.options": {
+      "extends": ["standard", "plugin:vue/essential"],
+      "plugins": ["html", "vue"],
+      "parserOptions": {
+          "sourceType": "module",
+          "parser": "babel-eslint"
+      },
+      "env": {
+          "browser": true,
+          "node": true
+      },
+  },
+}
+*/
+// npm install -g
+
+// npm i eslint-config-standard eslint-plugin-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-node eslint-plugin-html babel-eslint -g
