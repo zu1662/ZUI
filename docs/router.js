d155d5aed4c646089f48763e34484e7a64ab25b7
zu1662 <zu1662@163.com>
2019-07-25 16:32:51 +0800

    Update router.js

--- docs/router.js
+++ docs/router.js
@@ -1,24 +1,48 @@
-import { packages } from './doc.config.js'
+import { navigation } from './doc.config.js'
 
-const ua = navigator.userAgent.toLowerCase();
-const isMobile = /ios|iphone|ipod|ipad|android/.test(ua);
+// 根据不同环境设置routes
+function getRoutes(options) {
+  const isMobile = options.mobile
+  const routes = []
+  navigation.map(category => {
+    let groups = category.groups
+    if(category.showInMobile) {
+      groups.map(group => {
+        let lists = group.list
+        lists.map(list => {
+          const pkgName = list.name.toLowerCase()
+          if(isMobile) {
+            routes.push({
+              path: '/' + pkgName,
+              component: () => import(`../src/packages/${pkgName}/demo/index.vue`),
+              name: list.name
+            })
+          }else {
+            routes.push({
+              path: '/' + pkgName,
+              component: () => import(`../src/packages/${pkgName}/README.md`),
+              name: list.name
+            })
+          }
+        })
+      })
+    }else {
+      if (!isMobile) {
+        groups.map(group => {
+          let lists = group.list
+          lists.map(list => {
+            const pkgName = list.name.toLowerCase()
+            routes.push({
+              path: '/' + pkgName,
+              component: () => import(`../docs/markdown/` + pkgName + `.md`),
+              name: list.name
+            })
+          })
+        })
+      }
+    }
+  })
+  return routes
+}
 
-let routes = []
-
-packages.map(item => {
-  const pkgName =  item.name.toLowerCase();
-  if(isMobile) {
-    routes.push({
-      path: '/' + pkgName,
-      component: () => import('../src/packages/' + pkgName + '/demo/index.vue'),
-      name: item.name
-    });
-  }else {
-    routes.push({
-      path: '/' + pkgName,
-      component: () => import('../src/packages/' + pkgName + '/README.md'),
-      name: item.name
-    });
-  }
-})
-export default routes
+export default getRoutes
