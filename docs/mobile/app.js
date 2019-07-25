44ffa7f8291033b9849041740bf4bd878fedd2a2
zu1662 <zu1662@163.com>
2019-07-25 16:35:33 +0800

    Update app.js

--- docs/mobile/app.js
+++ docs/mobile/app.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import routes from '../router'
+import getRoutes from '../router'
 import App from './App.vue'
 
 // 阻止 vue 在启动时生成生产提示
-Vue.config.productionTip = false
+if (process.env.NODE_ENV !== 'production') {
+  Vue.config.productionTip = false;
+}
+
 Vue.use(VueRouter)
 const router = new VueRouter({
-  routes
+  routes: getRoutes({mobile: true}),
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { x: 0, y: 0 };
+  }
 });
 
+window.VueRouter = router
 
 new Vue({
   render: h => h(App),
